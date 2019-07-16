eab2ef9e8a54487645296c923ed7297f2f031fec
samuellye98 <sl2982@cornell.edu>
2019-07-16 11:24:39 +0800

    corsell logo

--- screens/AuthenticationScreen/LoginScreen.js
+++ screens/AuthenticationScreen/LoginScreen.js
@@ -39,7 +39,7 @@ class LoginScreen extends Component {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
         <Image
-          source={require("../../assets/images/robot-dev.png")}
+          source={require("../../assets/images/corsell-logo.png")}
           style={styles.logo}
         />
         <View style={styles.form}>
